b53f78da4f63ffb7935e1d563cbcbe04099cae00
Justin Dodson <justin@89websolutions.com>
2019-11-07 14:31:46 -0600

    updated helpers.py todo

--- Controller/helpers.py
+++ Controller/helpers.py
@@ -2,7 +2,7 @@ from datetime import datetime, timezone
 import sqlite3
 
 def convert_time_to_utc(date_string):
-    # get year, month, and day nums from date_string argument and set into test variable
+    # TODO: get year, month, and day nums from date_string argument and set into test variable
     date_list = date_string.split()
     test = datetime(int(date_list[0]), int(date_list[1]), int(date_list[2]), tzinfo=timezone.utc).timestamp()
     print(test)
