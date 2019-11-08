ff5751f3552de53471c561855c84328b4b2156ef
Justin Dodson <justin@89websolutions.com>
2019-11-08 13:21:22 -0600

    added network capabilites

--- Controller/MainController.py
+++ Controller/MainController.py
@@ -1,6 +1,9 @@
-from View.MainWindow import Ui_MainWindow
 from Controller.helpers import convert_time_to_utc
+from Controller.dialogs import show_connection_dialgo, show_scrape_error_dialog
 from Model.helpers import search_for_stock_ticker
+from Utils.web_scraper import StockData
+from View.MainWindow import Ui_MainWindow
+
 import sys
 from PyQt5 import QtWidgets
 
@@ -11,29 +14,32 @@ class MainControllerEXEC:
         # Create Application Instance
         app = QtWidgets.QApplication(sys.argv)
 
-        # Create new MainWindow Instance
-        MainWindow = QtWidgets.QMainWindow()
+        # Create new main_window Instance
+        main_window = QtWidgets.QMainWindow()
 
         # Create a ui instance to reference from the View
-        # and pass in the MainWindow as an argument to update the
+        # and pass in the main_window as an argument to update the
         # main window remotely
         self.ui = Ui_MainWindow()
-        self.ui.setupUi(MainWindow)
+        self.ui.setupUi(main_window)
+
+        self.date_dict = {}
 
         # call functions to process events
         self.trigger_date_change()
         self.trigger_search_bar_used()
         self.trigger_recent_stock_selection()
-        self.trigger_seach_button()
+        self.trigger_search_button()
+        self.trigger_import_date()
 
-
-        # Show MainWindow and execute application
-        MainWindow.show()
+        # Show main_window and execute application
+        main_window.show()
         sys.exit(app.exec_())
 
     """
         -- Signal methods --
     """
+
     def trigger_date_change(self):
         pass
 
@@ -43,8 +49,11 @@ class MainControllerEXEC:
     def trigger_recent_stock_selection(self):
         self.ui.recentStocksList.currentTextChanged.connect(self.update_search_bar)
 
-    def trigger_seach_button(self):
+    def trigger_search_button(self):
         self.ui.searchButton.clicked.connect(self.perform_search)
+
+    def trigger_import_date(self):
+        self.ui.importButton.clicked.connect(self.scrape_stock_data)
 
     """
         -- Class Methods --
@@ -69,18 +78,20 @@ class MainControllerEXEC:
         if self.ui.recentStocksList.currentText() != 'Select Recent':
             self.ui.stockSearchBar.setDisabled(True)
             self.ui.searchButton.setDisabled(True)
-        #     TODO: Create auto search method to populate date list
         else:
             self.ui.stockSearchBar.setDisabled(False)
             self.ui.searchButton.setDisabled(False)
 
     def perform_search(self):
+        self.ui.dateSelect.clear()
+        self.date_dict.clear()
         search_value = self.ui.stockSearchBar.text().upper()
         if search_value != '':
             result = search_for_stock_ticker(search_value)
             if result is not None:
                 self.ui.stockSearchResult.setText(result[1])
                 self.ui.stockSearchResult.repaint()
+                self.load_date_picker(search_value)
             else:
                 self.ui.stockSearchResult.setText('No Stock found with symbol \'{}\''.format(search_value))
                 self.ui.stockSearchResult.repaint()
@@ -90,4 +101,36 @@ class MainControllerEXEC:
 
     def date_selected(self):
         convert_time_to_utc(self.ui.dateSelect.date().toString('yyyy MM dd'))
+
+    def load_date_picker(self, stock_symbol):
+        stock = StockData(stock_symbol)
+        self.date_dict = stock.get_date_options()
+
+        if self.date_dict is not None:
+            for date in self.date_dict:
+                self.ui.dateSelect.addItem(date)
+                self.ui.dateSelect.repaint()
+        else:
+            show_connection_dialgo(self)
+
+    def scrape_stock_data(self):
+        date = self.date_dict[self.ui.dateSelect.currentText()]
+        stock = StockData(self.ui.stockSearchBar.text().upper())
+        stock_data = stock.get_call_data(date)
+        if stock_data is not None:
+            self.populate_data_table(stock_data)
+        else:
+            print('Issues HERE')
+
+    def populate_data_table(self, stock_data):
+        row_number = self.ui.stockTable.rowCount()
+        self.ui.stockTable.insertRow(row_number)
+
+        for i in range(len(stock_data)):
+            self.ui.stockTable.setItem(row_number, i, QtWidgets.QTableWidgetItem(stock_data[i]))
+            self.ui.stockTable.repaint()
+
+        self.ui.stockTable.setItem(row_number, 4, QtWidgets.QTableWidgetItem(self.ui.dateSelect.currentText()))
+
+        self.ui.stockTable.repaint()
 
---
+++ Controller/dialogs.py
@@ -0,0 +1,20 @@
+from PyQt5 import QtWidgets
+
+
+def show_connection_dialgo(self):
+    msg = QtWidgets.QMessageBox()
+    msg.setIcon(QtWidgets.QMessageBox.Critical)
+
+    msg.setText('No Network Connect')
+    msg.setInformativeText('The network is taking too long to load. Please try again later.')
+    msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+    msg.exec_()
+
+def show_scrape_error_dialog(self):
+    msg = QtWidgets.QMessageBox()
+    msg.setIcon(QtWidgets.QMessageBox.Warning)
+
+    msg.setText('Data Not Gathered')
+    msg.setInformativeText('The page took too long to load and was unable to retrieve the Call Data.')
+    msg.setStandardButtons(QtWidgets.QMessageBox.Ok)
+    msg.exec_()
